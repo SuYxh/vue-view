21a1bd9f8dce532a015ce1c887bb64be36a66476
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-09-06 13:43:14 +0800

    feat: update playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue';
 // @ts-ignore
 import DefineOptions from 'unplugin-vue-define-options/vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@yangxinhao/vue-view': path.resolve(__dirname, '../packages/vueView/index.ts')
+    }
+  },
+  // @ts-ignore
   plugins: [vue(), DefineOptions(), vueJsx()]
 });
